4a1eae80952e5d080a71b3b0f718b6721754dd43
ismailmail123 <ismailbary2@gmail.com>
2024-05-01 12:20:20 +0800

    menghilangkan reload

--- src/pages/DetailHistory/components/details/index.jsx
+++ src/pages/DetailHistory/components/details/index.jsx
@@ -15,12 +15,8 @@ import {
 const index = ({ item }) => {
   const { addCartItems } = useCartStore();
   const onAddToCart = () => {
-    window.location.reload();
-    addCartItems(item);
     // window.location.reload();
-    alert("data berhasil ditambahkan");
-    // navigate("/cart")
-    console.log("ini order historyyyyyyyy", item);
+    addCartItems(item);
   };
   return (
     <>
